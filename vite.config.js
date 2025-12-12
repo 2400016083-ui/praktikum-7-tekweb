7c748f8632855162bd4f180cc77085c4da22456b
Rifa Ananda Wahyuri <2400016083@webmail.uad.ac.id>
2025-12-12 19:31:57 +0700

    fix vite base

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import tailwindcss from "@tailwindcss/vite"; // Plugin v4 baru
+import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-})
+  base: "/praktikum-7-tekweb/", 
+});
